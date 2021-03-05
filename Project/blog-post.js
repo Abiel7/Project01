aa1904444125a228478f55d0a10874f8530b4711
abiel kidane <abielkidane19@hotmail.com>
2021-03-05 22:27:27 +0100

    dabasev2

---
+++ Project/blog-post.js
@@ -0,0 +1,13 @@
+export const query = graphql`
+    query($slug: String!) {
+        markdownRemark(fields: { slug: { eq: $slug } }) {
+            html
+            frontmatter {
+                title
+            }
+            fields {
+                slug
+            }
+        }
+    }
+`
